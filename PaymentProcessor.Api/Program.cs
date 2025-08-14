966f5ad7b8827af494415282e4b092c3dee550b9
Gessio Mori <gessiomorin@gmail.com>
2025-08-14 11:55:37 -0300

    Add project files.

---
+++ PaymentProcessor.Api/Program.cs
@@ -0,0 +1,29 @@
+
+namespace PaymentProcessor.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        WebApplication app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
